0c2a5effb81c5b604129f11bea9962b6f6e86e9a
afifa4300-sudo <afifa4300@gmail.com>
2026-09-12 05:45:59 +0700

    Add new Dosen data with NIDN 003 and update existing entries

--- app/Models/dosen.php
+++ app/Models/dosen.php
@@ -7,11 +7,18 @@ class Dosen
         return [
             [
                 'nidn' => '001',
-                'nama' => 'Bapak Ahmad'
+                'nama' => 'Bapak Ahmad',
+                'prodi' => 'Teknik Informatika',
             ],
             [
                 'nidn' => '002',
-                'nama' => 'Ibu Siti'
+                'nama' => 'Ibu Siti',
+                'prodi'=> 'Sistem Informasi'
+            ],
+            [
+                'nidn'  => '003',                  // ⬅️ data baru (dosen ke-3 sudah ditambahkan)
+                'nama'  => 'Budi',
+                'prodi' => 'Teknik Informatika'
             ]
         ];
     }
