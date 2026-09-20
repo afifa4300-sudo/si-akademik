55ddb144da5b0036a892ff871434b717aab19ecb
afifa4300-sudo <afifa4300@gmail.com>
2026-09-20 19:03:13 +0700

    Refactor routing in web.php to handle 'dosen' URL with direct controller invocation

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,9 @@
-<?php
-return [
+require_once __DIR__ . '/../app/Controllers/DosenController.php';
+require_once __DIR__ . '/../app/Middleware/AuthMiddleware.php';
 
-    'login' => [
-        'controller'  => 'AuthController',
-        'method'      => 'showLogin',
-        'middleware'  => [],
-    ],
-
-    'login/process' => [
-        'controller'  => 'AuthController',
-        'method'      => 'login',
-        'middleware'  => [],
-    ],
-
-    'logout' => [
-        'controller'  => 'AuthController',
-        'method'      => 'logout',
-        'middleware'  => [],
-    ],
-
-    'dashboard' => [
-        'controller'  => 'DashboardController',
-        'method'      => 'index',
-        'middleware'  => ['AuthMiddleware'],
-    ],
-
-    'mahasiswa' => [
-        'controller'  => 'MahasiswaController',
-        'method'      => 'index',
-        'middleware'  => ['AuthMiddleware'],
-    ],
-
-    'mahasiswa/detail' => [
-        'controller'  => 'MahasiswaController',
-        'method'      => 'detail',
-        'middleware'  => ['AuthMiddleware'],
-    ],
-
-    'dosen' => [
-        'controller'  => 'DosenController',
-        'method'      => 'index',
-        'middleware'  => ['AuthMiddleware'],
-    ],
-
-];
+if ($url === 'dosen') {
+ AuthMiddleware::handle();
+ $controller = new DosenController();
+ $controller->index();
+ exit;
+}
